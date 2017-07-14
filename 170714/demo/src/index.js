92a174792cbbc1c0a2700ee2729181394e81115b
yangjunjie <867656597@qq.com>
2017-07-14 17:23:46 +0800

    开始写评论功能 demo

--- 170714/demo/src/index.js
+++ 170714/demo/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class CommentApp extends React.Component {
+	render() {
+		return (
+			<div>
+
+			</div>
+		)
+	}
+
+}
+
+class COmment
+
+
+ReactDOM.render(
+	<CommentApp />,
+	document.getElementById('root')
+)
