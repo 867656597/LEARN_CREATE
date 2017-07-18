72545b05fbdc218e08c1c77a01676e104089e737
unknown <867656597@qq.com>
2017-07-19 00:31:44 +0800

    提交代码

--- 170714/demo/src/templet/CommentInput.jsx
+++ 170714/demo/src/templet/CommentInput.jsx
@@ -10,18 +10,32 @@ class CommentInput extends React.Component {
 			content: ''
 		}
 	}
+	handlerUserNameChange(event) {
+		this.setState({
+	      username: event.target.value
+	    })
+	}
+	handlerContentChange(event) {
+		this.setState({
+			content: event.target.value
+		})
+	}
 	render() {
 		return (<div className='coment-input'>
 					<div className="comment-field">
 						<span className='comment-field-name'>用户名:</span>
 						<div className = "comment-field-input">
-							<input value={this.state.username} />
+							<input 
+							onChange={this.handlerUserNameChange.bind(this)}
+							value={this.state.username} />
 						</div>
 					</div>
 					<div className="comment-field">
 						<span className='comment-field-name'>评论内容:</span>
 						<div className = "comment-field-input">
-							<textarea value={this.state.content} />
+							<textarea 
+							onChange={this.handlerContentChange.bind(this)}
+							value={this.state.content} />
 						</div>
 					</div>
 					<div className='comment-field-button'>
